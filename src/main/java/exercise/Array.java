d4f4d4c26ce185b9b94c38265c5ec6471a52a7a1
Goous1337 <127313390+Goous1337@users.noreply.github.com>
2024-03-17 19:22:19 +0300

    test task completed

--- src/main/java/exercise/Array.java
+++ src/main/java/exercise/Array.java
@@ -4,7 +4,7 @@ public class Array {
 
     public static void main(String [] args){
 
-        int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ;
+        int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int a;
         for (int j : array) {
             if ((j % 2) == 0) {
